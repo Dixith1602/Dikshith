658b01c59f0bc343d99b4f068cbe95a31fcd3409
Dixith G R <60172680+Dixith1602@users.noreply.github.com>
2025-11-21 02:16:19 +0530

    Update analytical and windows.sql

--- Sql/analytical and windows.sql
+++ Sql/analytical and windows.sql
@@ -1,6 +1,4 @@
-/*
-
-Assign row numbers to customers ordered by credit limit descending.
+--Assign row numbers to customers ordered by credit limit descending.
 
 select 
     ROW_NUMBER() over(order by cust_credit_limit desc) as ROW_NUMBER,
@@ -10,7 +8,7 @@ select
 from sh.customers ;
 
 
-Rank customers within each state by credit limit.
+--Rank customers within each state by credit limit.
 
 select 
     rank() OVER (PARTITION by cust_state_province order by cust_credit_limit) as rc,
@@ -18,7 +16,7 @@ select
     cust_state_province
 from sh.customers
 
-Use DENSE_RANK() to find the top 5 credit holders per country.
+--Use DENSE_RANK() to find the top 5 credit holders per country.
 
 select * from(select
     DENSE_RANK() over (partition by country_id order by cust_credit_limit desc) as DC,
@@ -27,7 +25,7 @@ select * from(select
 from sh.CUSTOMERS)
 where DC <= 5
 
-Divide customers into 4 quartiles based on their credit limit using NTILE(4).
+--Divide customers into 4 quartiles based on their credit limit using NTILE(4).
 
 select
     cust_id, 
@@ -35,7 +33,7 @@ select
     ntile(4) over (order by cust_credit_limit) as limitt
 from sh.customers;
 
-Calculate a running total of credit limits ordered by customer_id.
+--Calculate a running total of credit limits ordered by customer_id.
 
 select
     cust_id, 
@@ -43,7 +41,7 @@ select
     sum(CUST_CREDIT_LIMIT) over (order by cust_id)
 from sh.customers;
 
-Show cumulative average credit limit by country.
+--Show cumulative average credit limit by country.
 
 select
     country_id, 
@@ -51,7 +49,7 @@ select
     avg(CUST_CREDIT_LIMIT) over (order by country_id)
 from sh.customers;
 
-Compare each customer’s credit limit to the previous one using LAG().
+--Compare each customer’s credit limit to the previous one using LAG().
 
  select 
     cust_id,
@@ -59,7 +57,7 @@ Compare each customer’s credit limit to the previous one using LAG().
     lag(cust_credit_limit) over (order by cust_id) as lagging
 from sh.customers;
 
-Show next customer’s credit limit using LEAD().
+--Show next customer’s credit limit using LEAD().
 
  select 
     cust_id,
@@ -67,7 +65,7 @@ Show next customer’s credit limit using LEAD().
     lead(cust_credit_limit) over (order by cust_id) as leadings
 from sh.customers;
 
-Display the difference between each customer’s credit limit and the previous one.
+--Display the difference between each customer’s credit limit and the previous one.
 
  select 
     cust_id,
@@ -75,7 +73,7 @@ Display the difference between each customer’s credit limit and the previous o
     cust_credit_limit - lag(CUST_CREDIT_LIMIT, 1, 0) over (order by cust_id) as diff
 from sh.customers;
 
-For each country, display the first and last credit limit using FIRST_VALUE() and LAST_VALUE().
+--For each country, display the first and last credit limit using FIRST_VALUE() and LAST_VALUE().
 
  select 
     country_id,
@@ -84,7 +82,7 @@ For each country, display the first and last credit limit using FIRST_VALUE() an
     LAST_VALUE(cust_credit_limit) over (partition by country_id order by cust_credit_limit rows between unbounded preceding and unbounded following) as last_credit_value
 from sh.CUSTOMERS
 
-Compute percentage rank (PERCENT_RANK()) of customers based on credit limit.
+--Compute percentage rank (PERCENT_RANK()) of customers based on credit limit.
 
  select
     cust_id,
@@ -92,7 +90,7 @@ Compute percentage rank (PERCENT_RANK()) of customers based on credit limit.
     PERCENT_RANK() over (order by cust_credit_limit) as pecentage_rank
 from sh.customers 
 
-Show each customer’s position in percentile (CUME_DIST() function).
+--Show each customer’s position in percentile (CUME_DIST() function).
 
  select
     cust_id,
@@ -100,7 +98,7 @@ Show each customer’s position in percentile (CUME_DIST() function).
     CUME_DIST() over (order by cust_credit_limit) as cumedist
 from sh.customers
 
-Display the difference between the maximum and current credit limit for each customer.
+--Display the difference between the maximum and current credit limit for each customer.
 
  select 
     cust_id,
@@ -108,7 +106,7 @@ Display the difference between the maximum and current credit limit for each cus
     max(cust_credit_limit) over()- cust_credit_limit as diff
 from sh.customers 
 
-Rank income levels by their average credit limit.
+--Rank income levels by their average credit limit.
 
  select
     cust_income_level,
@@ -118,14 +116,14 @@ from sh.customers
 group by CUST_INCOME_LEVEL
 order by Ranking 
 
-Calculate the average credit limit over the last 10 customers (sliding window).
+--Calculate the average credit limit over the last 10 customers (sliding window).
  SELECT
   cust_id,
   cust_credit_limit,
   AVG(cust_credit_limit) OVER (ORDER BY cust_id ROWS BETWEEN 9 PRECEDING AND CURRENT ROW) AS avg_last_10
 FROM sh.customers
 
-For each state, calculate the cumulative total of credit limits ordered by city.
+--For each state, calculate the cumulative total of credit limits ordered by city.
 
  select 
     cust_city_id,
@@ -134,7 +132,7 @@ For each state, calculate the cumulative total of credit limits ordered by city.
     sum(cust_credit_limit) over (partition by CUST_STATE_PROVINCE order by CUST_CITY_ID) 
 from sh.customers
 
-Find customers whose credit limit equals the median credit limit (use PERCENTILE_CONT(0.5)).
+--Find customers whose credit limit equals the median credit limit (use PERCENTILE_CONT(0.5)).
 
  with median_value as(
     select 
@@ -149,7 +147,7 @@ from sh.customers, median_value
 where 
 customers.CUST_CREDIT_LIMIT = median_value.val;
 
-Display the highest 3 credit holders per state using ROW_NUMBER() and PARTITION BY.
+--Display the highest 3 credit holders per state using ROW_NUMBER() and PARTITION BY.
 
  select
     cust_id, cust_state_province, cust_credit_limit
@@ -162,7 +160,7 @@ from(
 where rn <= 3
 order by cust_state_province, rn
 
-Identify customers whose credit limit increased compared to previous row (using LAG).
+--Identify customers whose credit limit increased compared to previous row (using LAG).
 
  with customer_lag as(
     select 
@@ -177,7 +175,7 @@ from customer_lag
 where 
     CUST_CREDIT_LIMIT > lagging
 
-Calculate moving average of credit limits with a window of 3.
+--Calculate moving average of credit limits with a window of 3.
 
  select  
     cust_id,
@@ -185,7 +183,7 @@ Calculate moving average of credit limits with a window of 3.
     avg(cust_credit_limit) over (order by cust_id rows between 2 preceding and current row) as moving_avg
 from sh.customers
 
-Show cumulative percentage of total credit limit per country.
+--Show cumulative percentage of total credit limit per country.
 
  select
   country_id,
@@ -197,7 +195,7 @@ Show cumulative percentage of total credit limit per country.
 from sh.customers
 order by country_id, cust_credit_limit
 
-Rank customers by age (derived from CUST_YEAR_OF_BIRTH).
+--Rank customers by age (derived from CUST_YEAR_OF_BIRTH).
 
  select 
   cust_id,
@@ -206,7 +204,7 @@ Rank customers by age (derived from CUST_YEAR_OF_BIRTH).
   rank() over (order by extract(year from current_date) - cust_year_of_birth desc) as age_rank
 from sh.customers
 
-Calculate difference in age between current and previous customer in the same state.
+--Calculate difference in age between current and previous customer in the same state.
 
  with cust_age as (
   select
@@ -224,7 +222,7 @@ select
   age - lag(age) over (partition by cust_state_province order by age desc) as age_diff
 from cust_age
 
-Use RANK() and DENSE_RANK() to show how ties are treated differently.
+--Use RANK() and DENSE_RANK() to show how ties are treated differently.
 
  select
     cust_id,
@@ -233,7 +231,7 @@ Use RANK() and DENSE_RANK() to show how ties are treated differently.
     DENSE_RANK() over (order by cust_credit_limit desc) as densing
 from sh.customers 
 
-Compare each state’s average credit limit with country average using window partition.
+--Compare each state’s average credit limit with country average using window partition.
 
  select
   country_id,
@@ -243,7 +241,7 @@ Compare each state’s average credit limit with country average using window pa
   avg(cust_credit_limit) over (partition by country_id) as avg_credit_limit_country
 from sh.customers
 
-Show total credit per state and also its rank within each country.
+--Show total credit per state and also its rank within each country.
 
  select
   country_id,
@@ -254,7 +252,7 @@ from sh.customers
 group by country_id, CUST_STATE_PROVINCE
 order by country_id, rank_within_country
 
-Find customers whose credit limit is above the 90th percentile of their income level.
+--Find customers whose credit limit is above the 90th percentile of their income level.
 
  with income_percentiles as (
   select
@@ -272,7 +270,7 @@ select
 from income_percentiles
 where cust_credit_limit > credit_limit_90th_percentile
 
-Display top 3 and bottom 3 customers per country by credit limit.
+--Display top 3 and bottom 3 customers per country by credit limit.
 
  with rankedcustomers as (
   select
@@ -288,7 +286,7 @@ from rankedcustomers
 where rank_desc <= 3 or rank_asc <= 3
 order by country_id, rank_desc
 
-Calculate rolling sum of 5 customers’ credit limit within each country.
+--Calculate rolling sum of 5 customers’ credit limit within each country.
 
  select
   cust_id,
@@ -301,4 +299,3 @@ Calculate rolling sum of 5 customers’ credit limit within each country.
   ) as rolling_sum_5_credit_limit
 from sh.customers
 
-*/
