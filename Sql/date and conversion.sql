4936c34d664d6c0b7fcb2e98505853ac523374d4
Dixith G R <60172680+Dixith1602@users.noreply.github.com>
2025-11-22 13:49:50 +0530

    Update date and conversion.sql

--- Sql/date and conversion.sql
+++ Sql/date and conversion.sql
@@ -1,6 +1,4 @@
-/*
-
-1. Convert CUST_YEAR_OF_BIRTH to age as of today.
+--1. Convert CUST_YEAR_OF_BIRTH to age as of today.
 
 select 
     cust_id,
@@ -8,7 +6,7 @@ select
 from sh.customers
 order by cust_id 
 
-2. Display all customers born between 1980 and 1990.
+--2. Display all customers born between 1980 and 1990.
 
 select
     cust_id,
@@ -16,21 +14,21 @@ select
 from sh.customers 
 where cust_year_of_birth between 1980 and 1990
 
-3. Format must effective from into “Month YYYY” using TO_CHAR.
+--3. Format must effective from into “Month YYYY” using TO_CHAR.
 
 select 
     to_char(cust_eff_from, 'dd-mon-yyyy') 
 from sh.customers
 
-4. Convert income level text (like 'A: Below 30,000') to numeric lower limit.
-5. Display customer birth decades (e.g., 1960s, 1970s).
+--4. Convert income level text (like 'A: Below 30,000') to numeric lower limit.
+--5. Display customer birth decades (e.g., 1960s, 1970s).
 
 select 
     cust_id,
     floor(cust_year_of_birth/10) * 10 || 's' as decades
 from sh.customers
 
-6. Show customers grouped by age bracket (10-year intervals).
+--6. Show customers grouped by age bracket (10-year intervals).
 
 select
     cust_id,
@@ -51,14 +49,14 @@ case
 end as age_gap
 from sh.customers;
 
-7. Convert country_id to uppercase and state name to lowercase.
+--7. Convert country_id to uppercase and state name to lowercase.
 
 select 
     upper(country_id) as Upper_caps,
     lower(CUST_STATE_PROVINCE) as low_caps
 from sh.customers
 
-8. Show customers where credit limit > average of their birth decade.
+--8. Show customers where credit limit > average of their birth decade.
 
 WITH decade_avg AS (
     SELECT 
@@ -77,16 +75,15 @@ JOIN decade_avg d ON
     FLOOR(c.cust_year_of_birth /10) *10 = d.birth_decade
     WHERE c.cust_credit_limit > d.avg_credit_limit;
 
-9. Convert all numeric credit limits to currency format $999,999.00.
+--9. Convert all numeric credit limits to currency format $999,999.00.
 
 select
     CUST_CREDIT_LIMIT,
     to_char(cust_credit_limit, '$999,999.00’) as formatted_credit_limit
-from sh.customers
+from sh.customers;
 
-10. Find customers whose credit limit was NULL and replace with average (using NVL).
+--10. Find customers whose credit limit was NULL and replace with average (using NVL).
 
 select
     NVL(cust_credit_limit, (select avg(cust_credit_limit) from sh.customers)) as updated_credit_limit
 from sh.customers
-*/
